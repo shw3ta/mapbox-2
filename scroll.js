d55d9fd49caa255dade1122a821d7d4af22c3817
shw3ta <shweta.prasad_ug21@ashoka.edu.in>
2022-05-04 14:22:05 +0530

    clean up

--- scroll.js
+++ scroll.js
@@ -7,149 +7,194 @@ zoom: 3,
 bearing: 0,
 pitch: 0
 });
-//This is all the stuff that runs on the first load of the map.
-map.on('load', () => {
-  //Hide all presentation layers
-  //This demo uses three specific layers. I want to hide them initially so I can reveal them piece meal.
-  map.setLayoutProperty('temporality-count', 'visibility', 'none');
-  map.setLayoutProperty('gendered-lines', 'visibility', 'none');
-  
 
-  //Hide the legend, slider, and infobox on first load. Obviously delete these lines if you want them visible from the start.
-  document.getElementById('legend').style.display = 'none';
+/////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////
+
+map.on('load', () => 
+{
+  // I want to show the slider, legend from the start
+  map.setLayoutProperty('lines-female','visibility', 'visible');
+  map.setLayoutProperty('lines-male', 'visibility', 'visible');
+
+  // don't show the console and slider unless the page is scrolled to part_3
   document.getElementById('console').style.display = 'none';
 
-  //to reduce clutter, the steps for creating a legend, slider, and menu have all been turned into functions.
-  createLegend()
-  createSlider()
-  createMenu()
 
-});
+  ////////////////////////////////////////////////
+  // Legend
+  Legend()
 
-//This is a lazy function to hide and show menus relative to the layers. It waits for any change in the map rendering and then checks to see what menu items are active and turns on the infobox, slider, and legend. Normally, you would build this logic into the click event handler for each button.
+  ////////////////////////////////////////////////
+  // I want the user to be able to toggle between genders
+  // The gender buttons will ideally have the same colours as their legend
 
-map.on('idle', () => {
-
-  var toggleableLayerIds = ['gendered-lines',  'temporality-count'];
-
-  for (var i = 0; i < toggleableLayerIds.length; i++) {
-    var id = toggleableLayerIds[i];
-    var visibility = map.getLayoutProperty(id, 'visibility');
-
-    if (id == 'gendered-lines' && visibility === 'none') {
-      document.getElementById('legend').style.display = 'none';
-    } else if (id == 'gendered-lines' && visibility === 'visible') {
-      document.getElementById('legend').style.display = 'initial';
-    }
-    if (id == 'temporality-count' && visibility === 'none') {
-      document.getElementById('console').style.display = 'none';
-    } else if (id == 'temporality-count' && visibility === 'visible') {
-      document.getElementById('console').style.display = 'initial';
-    }
-   
-  }
-});
-
-function createMenu(){
-
-    // MENU For selecting layers
-    // Read in all the layers you want to toggle
-    var toggleableLayerIds = ['gendered-lines', 'temporality-count'];
-
-    //These are the names for the layers that will appear on the menu
-    var layerNames = ['Legend: Gender', 'Slider: Temporality']
-
-    //Loop that generates a menu item for each layer in the above array.
-    for (var i = 0; i < toggleableLayerIds.length; i++) {
-      var id = toggleableLayerIds[i];
-      var name = layerNames[i];
-      var link = document.createElement('a');
-      link.href = '#';
-      link.className = ''; //Menu initially sets every item as inactive.
-      link.textContent = name;
-      link.id = id;
-
-      //create an event handler for each menu item. If clicked check whether the layer is visible, if so set visibility to 'none' and vice versa.
-      link.onclick = function(e) 
-      {
-        var clickedLayer = this.id;
-        e.preventDefault();
-        e.stopPropagation();
-
-        var visibility = map.getLayoutProperty(clickedLayer, 'visibility');
-
-        if (visibility === 'visible') {
-          map.setLayoutProperty(clickedLayer, 'visibility', 'none');
-          this.className = '';
-
-        } else {
-          this.className = 'active';
-          map.setLayoutProperty(clickedLayer, 'visibility', 'visible');
-        }
-      };
-      var layers = document.getElementById('menu');
-      layers.appendChild(link);
-    }
-
+  // show menu buttons
+  Menu()
+  
 }
+);
+
+/////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////
 
 
-function createLegend() {
-  //LEGEND TEXT
-  //the var layers array sets the text that will show up in the legend. you can enter any value here it is just text. Make sure that the legend values correspond to the ones you set in Mapbox.
-  var layers = ['Female', 'Male'];
-
-  //LEGEND COLORS
-  //Set the corresponding LEGEND colors using HEX the easiest way to do this is by setting your mapcolors in Mapbox using ColorBrewer (colorbrewer2.org). Then copy the exact same hex value to the array below. Remember that each label above should correspond to a color. If the number of items in layers does not match the number of values in colors you will get an error.
-  var colors = ['#34c53c', '#2b1ba1'];
-
-//run through each element in the legend array and create a new legend item.
-  for (i = 0; i < layers.length; i++) {
-    var layer = layers[i];
-    var color = colors[i];
+// Function to fill the map legend
+function Legend()
+{
+  var legendElements = ['Female', 'Male'];
+  var legendColors = ['#34c53c', '#2b1ba1'];
+  for (var i = 0; i < 2; i++)
+  {
+    var legendElement = legendElements[i];
+    var elementColor = legendColors[i];
     var item = document.createElement('div');
     var key = document.createElement('span');
     key.className = 'legend-key';
-    key.style.backgroundColor = color;
+    key.style.backgroundColor = elementColor;
 
     var value = document.createElement('span');
-    value.innerHTML = layer;
+    value.innerHTML = legendElement;
     item.appendChild(key);
     item.appendChild(value);
+
     legend.appendChild(item);
   }
-  //LEGEND CODE
-
-
 }
 
-function createSlider() {
-  //Set the initial view at the first value. In this case, 1 for Pre-Partition.
-  map.setFilter('temporality-count', ['==', ['number', ['get', 'temporal_sequence']], 1]);
-  document.getElementById('active-temporality').innerText = 'Pre-Partition (before 1947-08-14)'
-  map.setLayoutProperty('temporality-count', 'visibility', 'none')
 
-  //Create event listener to catch whenever the slider is moved.
-  document.getElementById('slider').addEventListener('input', function(e) {
-    //get the value of the movement.
-    var step = parseInt(e.target.value, 10);
+// Function to populate the menu to toggle between filters to view male and female paths
+function Menu()
+{
+  var keys = ['lines-female', 'lines-male'];
+  var buttonLabels = ['Hide Paths of Females', 'Hide Paths of Males'];
+  
+  for (var i = 0; i < 2; i++)
+  {
+    var id = keys[i];
+    var buttonLabel = buttonLabels[i];
+    var link = document.createElement('a');
 
-    //These labels were created to populate the active temporality label. Change them if you are going with your own string sequence.
-    var label = ['Pre-Partition (before 1947-08-14)',
-      'Partition (1947-08-15 - 1948-02-28)',
-      'Post-Partition (1948-03-01 - 1971-12-16)',
-      'Long Partition (after 1971-12-16)',
-      'Indeterminable'
-    ]
+    // link properties
+    link.href = '#';
+    link.className = ''; // means it is initially inactive
+    link.textContent = buttonLabel;
+    link.id = id;
 
-    //This is the filter function, it relies on the layer name, the comparison operator (==), the first value which it grabs with the get, temporal sequence function, and then the thing being compared against (step), or the step in the sequence of the slider.
+    // create an event handler for each menu item
+    // if clicked, check the visibility of the layer
+    // if visibility is 'none' set it to 'visible' and vice versa
+    link.onclick = function(e) 
+    {
+      var clickedLayer = this.id;
+      e.preventDefault();
+      e.stopPropagation();
+      gender = this.textContent.split(" ")[3];
 
-    map.setFilter('temporality-count', ['==', ['number', ['get', 'temporal_sequence']], step]);
-    //This sets the label above the slider to the period value.
-    document.getElementById('active-temporality').innerText = label[step - 1] //+ ampm;
+      var visibility = map.getLayoutProperty(clickedLayer, 'visibility');
+      if (visibility == 'visible')
+      {
+        map.setLayoutProperty(clickedLayer, 'visibility', 'none');
+        this.className = '';
+        this.textContent = 'Show Paths of '.concat(gender) ;
+      }
+
+      else
+      {
+        this.className = 'active';
+        map.setLayoutProperty(clickedLayer, 'visibility', 'visible');
+        this.textContent = 'Hide Paths of '.concat(gender);
+      }
+    };
+
+    var layers = document.getElementById('menu');
+    layers.appendChild(link);
+  }
+}
+
+// Function that effects what the slider does and console shows.
+function Slider()
+{
+  var labels = ['Pre-Partition (before 1947-08-14)','Partition (1947-08-15 - 1948-02-28)','Post-Partition (1948-03-01 - 1971-12-16)','Long Partition (after 1971-12-16)','Indeterminable', 'Temporality not coded'];
+  
+  // showing the Pre-Partition condition by default
+  if (document.getElementById('active-temporality').innerText == '')
+  {
+    document.getElementById('active-temporality').innerText = 'Please move the slider.'
+  }
+  
+  // creating event listener to catch slider movement
+  document.getElementById('slider').addEventListener('input', function(e)
+  {
+    // where is the slider now?
+    var step = parseInt(e.target.value, 10) // in decimal
+
+    activeLayers = visibleLayers();
+    console.log(activeLayers);
+    for (var i = 0; i < activeLayers.length; i++)
+    {
+      ID = activeLayers[i];
+      gender = ID.split("-")[1];
+      gender = gender[0].toUpperCase() + gender.substring(1).toLowerCase();
+      console.log(ID)
+      console.log(gender)
+
+      const filterTemporality = ['==', ['number', ['get', 'temporal_sequence']], step];
+      const filterGender = ['==', ['string', ['get', 'gender']], gender];
+
+      map.setFilter(ID, ['all', filterGender, filterTemporality]);
+      document.getElementById('active-temporality').innerText = labels[step - 1];
+    }
   })
 }
 
+// naive approach, but it's guaranteed to work.
+function visibleLayers()
+{
+  var visibleLayerIds = [];
+
+  // case 1: both are visible
+  if (map.getLayoutProperty('lines-female', 'visibility') == 'visible' && map.getLayoutProperty('lines-male', 'visibility') == 'visible')
+  {
+    visibleLayerIds = ['lines-female', 'lines-male'];  
+  }
+  // case 2: only female layer is visible
+  else if (map.getLayoutProperty('lines-female', 'visibility') == 'visible')
+  {
+    visibleLayerIds = ['lines-female'];
+  }
+  // case 3: only male layer is visible
+  else
+  {
+    visibleLayerIds = ['lines-male'];
+  }
+
+  return visibleLayerIds;
+}
+
+// Function to set the active chapter to active
+function setActiveChapter(chapterName) 
+{
+  if (chapterName === activeChapterName) return;
+
+  map.flyTo(chapters[chapterName]);
+
+  document.getElementById(chapterName).classList.add('active');
+  document.getElementById(activeChapterName).classList.remove('active');
+
+  activeChapterName = chapterName;
+}
+
+
+// Function that tells you if the element of interest is within the bounds of the screen
+function isElementOnScreen(id) 
+{
+  const element = document.getElementById(id);
+  const bounds = element.getBoundingClientRect();
+  return bounds.top < window.innerHeight && bounds.bottom > 0;
+}
+
+//////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////
+
+// defining properties of chapter elements
 const chapters = {
 'part_1': {
 bearing:0,
@@ -165,11 +210,16 @@ zoom: 10,
 pitch: 0
 },
 'part_3': {
-bearing: 45,
-center: [74.3383, 31.5767],
-zoom: 13,
-speed: 1,
-pitch: 50
+
+bearing:0,
+center: [77.0688997, 28.5272803],
+zoom: 3,
+pitch: 0
+// bearing: 45,
+// center: [74.3383, 31.5767],
+// zoom: 13,
+// speed: 1,
+// pitch: 50
 },
 'part_4': {
 bearing: 45,
@@ -193,30 +243,36 @@ zoom: 7
 }
 };
 
+
+
 let activeChapterName = 'part_1';
-function setActiveChapter(chapterName) {
-if (chapterName === activeChapterName) return;
-
-map.flyTo(chapters[chapterName]);
-
-document.getElementById(chapterName).classList.add('active');
-document.getElementById(activeChapterName).classList.remove('active');
-
-activeChapterName = chapterName;
-}
-
-function isElementOnScreen(id) {
-const element = document.getElementById(id);
-const bounds = element.getBoundingClientRect();
-return bounds.top < window.innerHeight && bounds.bottom > 0;
-}
-
 // On every scroll event, check which element is on screen
 window.onscroll = () => {
-for (const chapterName in chapters) {
-if (isElementOnScreen(chapterName)) {
-setActiveChapter(chapterName);
-break;
-}
-}
+
+  for (const chapterName in chapters) 
+  {
+    
+    if (isElementOnScreen(chapterName)) 
+      {
+        // we want the slider and console to show up only from chapter 3 onwards
+        if (chapterName != 'part_1' && chapterName != 'part_2')
+        {
+          document.getElementById('console').style.display = 'inline';
+          document.getElementById('slider').style.display = 'inline';          
+          // activate slider functionality
+          Slider();
+      
+        }
+
+        // in chapters 1, 2, we hide the slider and the console.
+        else
+        {
+          document.getElementById('console').style.display = 'none';
+          document.getElementById('slider').style.display = 'none';
+        }
+        setActiveChapter(chapterName);
+        break; 
+        
+      }
+  }
 };
